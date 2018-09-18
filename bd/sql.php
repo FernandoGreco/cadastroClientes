8f2bcfaaca514fa2c69548a342f73ac5731a634d
Fernando Greco <greco-fernando@hotmail.com>
2018-09-18 01:34:28 -0300

    teste cadastro

--- bd/sql.php
+++ bd/sql.php
@@ -10,13 +10,17 @@ $sql_dividas = "SELECT id,nome,valor FROM cliente t1 inner join divida t2 on t1.
 $resultadoDividas = $conn->query($sql_dividas);
 
 
-//se for enviado só o nome então só cadastra 
+//cadastra nome e divida
 if (@$_POST['name'] && @$_POST['divida']) {
-	$nome = $_POST['name'];
-	$divida = $_POST['divida'];
-	echo "Nome".$nome."Divida:".$divida;
+	$nome = mysql_escape_string($_POST['name']);
+	$divida = mysql_escape_string($_POST['divida']);
+	$insert_nome = "INSERT INTO cliente (nome) VALUES ('$nome')";
+	$insert_divida = "INSERT INTO divida (valor) VALUES ('$divida')";
+	$conn->	query ($insert_nome);
+	$conn->	query ($insert_divida);
 }
 
+//cadastra só nome
 else if (@$_POST['name']) {
  $nome = mysql_escape_string($_POST['name']);
 $insert_nome = "INSERT INTO cliente (nome) VALUES ('$nome')";
